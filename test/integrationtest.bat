08e2b19641d7679eeff89a4162414d09176851f3
Marcus von Appen <marcus@sysfault.org>
2018-03-28 18:52:13 +0100

    - added integration test setup

---
+++ test/integrationtest.bat
@@ -0,0 +1,48 @@
+@ECHO OFF
+
+SETLOCAL
+SET FAILED=0
+SET PNAME=docprocci
+SET CIP=docprocci_docproc
+SET DOCKER=docker
+SET DOCKER_COMPOSE=docker-compose
+
+ECHO Building docker environment...
+%DOCKER% build -t docproc/base .
+%DOCKER_COMPOSE% -p %PNAME% build
+
+ECHO Starting docker environment...
+%DOCKER_COMPOSE% -p %PNAME% up -d
+
+ECHO Creating queues manually to speed up testing...
+%DOCKER% exec -d %CIP%.fileinput_1 curl -X POST http://127.0.0.1:4151/topic/create?topic=input
+%DOCKER% exec -d %CIP%.preproc_1 curl -X POST http://127.0.0.1:4151/topic/create?topic=preprocessed
+%DOCKER% exec -d %CIP%.renderer_1 curl -X POST http://127.0.0.1:4151/topic/create?topic=rendered
+%DOCKER% exec -d %CIP%.output_1 curl -X POST http://127.0.0.1:4151/topic/create?topic=output
+
+ECHO Starting tests...
+%DOCKER% cp examples/data/testrecords.csv %CIP%.fileinput_1:/app/data
+
+TIMEOUT /T 10
+
+REM DO NOT USE: the following lines are to sync proper results with the test result dir
+REM %DOCKER% exec %CIP%.output_1 ls -al /app/output
+REM %DOCKER% cp %CIP%.output_1:/app/output/. ./test/results
+
+%DOCKER% cp test/test-results.tar.gz %CIP%.output_1:/app
+%DOCKER% exec %CIP%.output_1 tar -C /app -xzf test-results.tar.gz
+%DOCKER% exec -it %CIP%.output_1 diff -Nur /app/output /app/test-results
+
+IF %ERRORLEVEL% NEQ 0 (
+    SET FAILED=1
+)
+
+%DOCKER_COMPOSE% -p %PNAME% kill
+%DOCKER_COMPOSE% -p %PNAME% rm -f
+
+IF %FAILED% == 0 (
+    ECHO Tests successful
+) ELSE (
+    ECHO Tests failed!
+)
+EXIT /B %FAILED%
---
+++ test/integrationtest.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e
+
+CIP="docprocci_docproc"
+PNAME="docproc-ci"
+DOCKER=docker
+DOCKER_COMPOSE=docker-compose
+
+echo "Building docker environment..."
+$DOCKER build -t docproc/base .
+$DOCKER_COMPOSE -p $PNAME build
+
+echo "Starting docker environment..."
+$DOCKER_COMPOSE -p $PNAME up -d
+
+echo "Creating queues manually to speed up testing..."
+$DOCKER exec -d $CIP.fileinput_1 curl -X POST http://127.0.0.1:4151/topic/create?topic=input
+$DOCKER exec -d $CIP.preproc_1 curl -X POST http://127.0.0.1:4151/topic/create?topic=preprocessed
+$DOCKER exec -d $CIP.renderer_1 curl -X POST http://127.0.0.1:4151/topic/create?topic=rendered
+$DOCKER exec -d $CIP.output_1 curl -X POST http://127.0.0.1:4151/topic/create?topic=output
+
+echo "Starting tests..."
+$DOCKER cp examples/data/testrecords.csv $CIP.fileinput_1:/app/data
+
+sleep 10
+
+# DO NOT USE: the following lines are to sync proper results with the test result dir
+# $DOCKER exec $CIP.output_1 ls -al /app/output
+# $DOCKER cp $CIP.output_1:/app/output/. ./test/results
+
+$DOCKER cp ./test/test-results.tar.gz $CIP.output_1:/app
+$DOCKER exec $CIP.output_1 tar -C /app -xzf test-results.tar.gz
+$DOCKER exec -it $CIP.output_1 diff -Nur /app/output /app/results
+exitcode=$?
+
+$DOCKER_COMPOSE -p $PNAME kill
+$DOCKER_COMPOSE -p $PNAME rm -f
+
+if [ $exitcode -ne 0 ]; then
+    echo "Tests failed!"
+else
+    echo "Tests successful"
+fi
+exit $exitcode
